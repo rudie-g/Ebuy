d96b703eb1dcd9e57d1d7ae46ca9bd54debbf329
Rudie G <rauoldm@gmail.com>
2021-07-07 16:26:48 -0500

    Fixed a seeding issue

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,6 +6,12 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     product_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -17,9 +23,9 @@ ProductTag.init(
       type: DataTypes.INTEGER,
       references: {
         model: "product",
-        key: "id"
-      }
-    }
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
