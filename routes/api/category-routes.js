a6bbb5e2d5b9c9aacec77fc3ccff0b9ab3b27364
Rudie G <rauoldm@gmail.com>
2021-07-07 17:06:02 -0500

    Fixed delete function

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -63,7 +63,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const categoryData = await Category.destroy(req.params.id, {
+    const categoryData = await Category.destroy({
       where: {
         id: req.params.id
       },
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -110,7 +110,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData = await Product.destroy(req.params.id, {
+    const productData = await Product.destroy({
       where: {
         id: req.params.id
       },
