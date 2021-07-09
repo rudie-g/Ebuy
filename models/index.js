67106002d0718fe600348e1ffd4183fdd51b186e
Rudie G <rauoldm@gmail.com>
2021-07-08 22:37:43 -0500

    Fixed DELETE issues

--- models/index.js
+++ models/index.js
@@ -5,20 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: "category_id",
-});
+Product.belongsTo(Category, { foreignKey: "category_id" });
 
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: "category_id",
-  onDelete: "CASCADE"
-});
+Category.hasMany(Product, { foreignKey: "category_id" });
 
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
 
 
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tag_id" });
 
 module.exports = {
   Product,
